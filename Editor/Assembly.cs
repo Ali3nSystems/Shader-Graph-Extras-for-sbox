19ffb1cbe61f1505ca0d0f06dde9047cc608d7e6
eEight-dev <120290229+eEight-dev@users.noreply.github.com>
2025-11-05 08:10:09 +0200

    Update Assembly.cs

--- Editor/Assembly.cs
+++ Editor/Assembly.cs
@@ -1,4 +1,6 @@
 global using Sandbox;
 global using Editor;
+global using System;
+global using System.Runtime.CompilerServices;
 global using System.Collections.Generic;
-global using System.Linq;
+global using System.IO;
