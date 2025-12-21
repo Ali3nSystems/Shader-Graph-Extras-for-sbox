10d8580cbdb344d142f5f81f4ce144185d59b2a2
eEight-dev <120290229+eEight-dev@users.noreply.github.com>
2025-12-22 00:10:33 +0200

    Add Blendable.cs and DeferredDecal.cs (tutorials soon)

---
+++ Editor/templates/Blendable.cs
@@ -0,0 +1,127 @@
+namespace Editor.ShaderGraph;
+public static class SGEBlendableTemplate
+{
+	public static Dictionary<string, bool> Features => new()
+	{
+		{ "SupportsAlbedo", true},
+		{ "SupportsEmission", true },
+		{ "SupportsOpacity", true},
+		{ "SupportsNormal", true },
+		{ "SupportsRoughness", true },
+		{ "SupportsMetalness", true },
+		{ "SupportsAmbientOcclusion", true },
+		{ "SupportsPositionOffset", true },
+        { "SupportsPixelDepthOffset", true},
+
+        {"SupportsLitShadingModel", true},
+		{"SupportsUnlitShadingModel", true},
+		{"SupportsCustomShadingModel", true},
+
+		{"SupportsOpaqueBlendMode", true},
+		{"SupportsMaskedBlendMode", true},
+		{"SupportsTranslucentBlendMode", true},
+		{"SupportsDynamicBlendMode", true},
+	};
+
+	public static string Code => @"
+HEADER
+{{
+	Description = ""{0}"";
+}}
+
+FEATURES
+{{
+	#include ""common/features.hlsl""
+	Feature( F_MULTIBLEND, 0..3 ( 0=""1 Layers"", 1=""2 Layers"", 2=""3 Layers"", 3=""4 Layers"" ), ""Number Of Blendable Layers"" );
+	{1}
+
+}}
+
+MODES
+{{
+	Forward();
+	Depth(S_MODE_DEPTH);
+	ToolsShadingComplexity( ""tools_shading_complexity.shader"" );
+}}
+
+COMMON
+{{
+{2}
+	#include ""common/shared.hlsl""
+	#include ""procedural.hlsl""
+
+	#define S_UV2 1
+	#define CUSTOM_MATERIAL_INPUTS
+}}
+
+struct VertexInput
+{{
+	#include ""common/vertexinput.hlsl""
+	float4 vColorBlendValues : TEXCOORD4 < Semantic( VertexPaintBlendParams ); >;
+	float4 vColorPaintValues : TEXCOORD5 < Semantic( VertexPaintTintColor ); >;
+	float4 vColor : COLOR0 < Semantic( Color ); >;
+{3}
+}};
+
+struct PixelInput
+{{
+	#include ""common/pixelinput.hlsl""
+	float4 vBlendValues		 : TEXCOORD14;
+	float4 vPaintValues		 : TEXCOORD15;
+	float4 vColor : COLOR0;
+{4}
+}};
+
+VS
+{{
+	StaticCombo( S_MULTIBLEND, F_MULTIBLEND, Sys( PC ) );
+
+	#include ""common/vertex.hlsl""
+
+	BoolAttribute( VertexPaintUI2Layer, F_MULTIBLEND == 1 );
+	BoolAttribute( VertexPaintUI3Layer, F_MULTIBLEND == 2 );
+	BoolAttribute( VertexPaintUI4Layer, F_MULTIBLEND == 3 );
+	BoolAttribute( VertexPaintUIPickColor, true );
+	BoolAttribute( ShadowFastPath, true );
+
+{5}{6}{7}
+	PixelInput MainVs( VertexInput v )
+	{{
+		PixelInput i = ProcessVertex( v );
+		i.vBlendValues = v.vColorBlendValues;
+		i.vPaintValues = v.vColorPaintValues;
+	
+		return FinalizeVertex( i );
+	}}
+}}
+
+PS
+{{
+	StaticCombo( S_MULTIBLEND, F_MULTIBLEND, Sys( PC ) );
+
+	#include ""common/pixel.hlsl""
+{8}{9}{10}
+	float4 MainPs( PixelInput i ) : SV_Target0
+	{{
+		Material m = Material::Init();
+		m.Albedo = float3( 1, 1, 1 );
+		m.Normal = float3( 0, 0, 1 );
+		m.Roughness = 1;
+		m.Metalness = 0;
+		m.AmbientOcclusion = 1;
+		m.TintMask = 1;
+		m.Opacity = 1;
+		m.Emission = float3( 0, 0, 0 );
+		m.Transmission = 0;
+{11}
+		m.AmbientOcclusion = saturate( m.AmbientOcclusion );
+		m.Roughness = saturate( m.Roughness );
+		m.Metalness = saturate( m.Metalness );
+		m.Opacity = saturate( m.Opacity );
+
+		// Result node takes normal as tangent space, convert it to world space now
+		m.Normal = TransformNormal( m.Normal, i.vNormalWs, i.vTangentUWs, i.vTangentVWs );
+{12}
+	}}
+}}";
+}
---
+++ Editor/templates/DeferredDecal.cs
@@ -0,0 +1,192 @@
+namespace Editor.ShaderGraph;
+
+public static class SGEDeferredDecalTemplate
+{
+	public static Dictionary<string, bool> Features => new()
+	{
+		{ "SupportsAlbedo", true},
+		{ "SupportsEmission", true },
+		{ "SupportsOpacity", true},
+		{ "SupportsNormal", true },
+		{ "SupportsRoughness", true },
+		{ "SupportsMetalness", true },
+		{ "SupportsAmbientOcclusion", true },
+		{ "SupportsPositionOffset", false },
+		{ "SupportsPixelDepthOffset", false},
+
+        {"SupportsLitShadingModel", true},
+		{"SupportsUnlitShadingModel", true},
+		{"SupportsCustomShadingModel", true},
+
+		{"SupportsOpaqueBlendMode", false},
+		{"SupportsMaskedBlendMode", false},
+		{"SupportsTranslucentBlendMode", true},
+		{"SupportsDynamicBlendMode", false}
+	};
+
+	public static string Code => @"
+HEADER
+{{
+	Description = ""{0}"";
+}}
+
+FEATURES
+{{
+	#include ""common/features.hlsl""
+{1}
+}}
+
+MODES
+{{
+	Forward();
+	Depth();
+	ToolsShadingComplexity( ""tools_shading_complexity.shader"" );
+}}
+
+COMMON
+{{
+{2}
+	#include ""common/shared.hlsl""
+	#include ""procedural.hlsl""
+}}
+
+struct VertexInput
+{{
+	#include ""common/vertexinput.hlsl""
+	float4 vColor : COLOR0 < Semantic( Color ); >;
+{3}
+}};
+
+struct PixelInput
+{{
+	#include ""common/pixelinput.hlsl""
+	float3 vDecalOrigin : TEXCOORD14;
+	float3 vDecalRight : TEXCOORD15;
+	float3 vDecalUp : TEXCOORD16;
+	float3 vDecalForward : TEXCOORD17;
+	float3 vDecalScale : TEXCOORD18;
+	float3 vPositionOs : TEXCOORD19;
+	float4 vColor : COLOR0;
+	float4 vTintColor : COLOR1;
+{4}
+}};
+
+VS
+{{
+	#include ""common/vertex.hlsl""
+	#include ""instancing.fxc""
+{5}{6}{7}
+	PixelInput MainVs( VertexInput i )
+	{{
+		float3x4 matObjectToWorld = GetTransformMatrix( i.nInstanceTransformID );
+
+		float3 vRight = mul( matObjectToWorld, float4( 1, 0, 0, 0 ) );
+		float3 vUp = mul( matObjectToWorld, float4( 0, 1, 0, 0 ) );
+		float3 vForward = mul( matObjectToWorld, float4( 0, 0, 1, 0 ) );
+
+		float3 vScale = float3( length( vRight ), length( vUp ), length( vForward ) );
+
+		float3 vBoundsHalfSize = abs( i.vPositionOs.xyz );
+
+		vBoundsHalfSize *= vScale;
+
+		float projectionDepth = 1;
+		float3 vScaledPositionOs = i.vPositionOs.xyz;
+		vScaledPositionOs.x *= projectionDepth;
+		vBoundsHalfSize.x *= projectionDepth;
+
+		float3 vPositionWs = mul( matObjectToWorld, float4( vScaledPositionOs, 1.0 ) );
+
+		PixelInput o = ProcessVertex( i );
+
+		o.vPositionOs = i.vPositionOs.xyz;
+		o.vColor = i.vColor;
+
+		ExtraShaderData_t extraShaderData = GetExtraPerInstanceShaderData( i.nInstanceTransformID );
+		o.vTintColor = extraShaderData.vTint;
+
+		o.vPositionPs = Position3WsToPs( vPositionWs );
+
+		o.vDecalOrigin = mul( matObjectToWorld, float4( 0, 0, 0, 1.0 ) );
+
+		o.vDecalScale = vBoundsHalfSize;
+
+		o.vDecalRight = vRight / vScale.x;
+		o.vDecalUp = vUp / vScale.y;
+		o.vDecalForward = vForward / vScale.z;
+
+		return FinalizeVertex( o );
+	}}
+}}
+
+PS
+{{
+	#include ""common/pixel.hlsl""
+	#include ""common/classes/Depth.hlsl""
+{8}{9}{10}
+	RenderState( BlendEnable, true );
+	RenderState( SrcBlend, SRC_ALPHA );
+	RenderState( DstBlend, INV_SRC_ALPHA );
+	RenderState( DepthWriteEnable, false );
+	RenderState( DepthEnable, false );
+	RenderState( CullMode, Front );
+
+	float4 MainPs( PixelInput i ) : SV_Target0
+	{{
+		float3 worldPos = Depth::GetWorldPosition( i.vPositionSs.xy );
+
+		float3 vDelta = worldPos - i.vDecalOrigin;
+
+		float3 vProjected = float3( dot( vDelta, i.vDecalRight ), dot( vDelta, i.vDecalUp ), dot( vDelta, i.vDecalForward ) );
+
+		float3 vHalfBounds = i.vDecalScale;
+		float3 vDecalBounds = vHalfBounds * 2.0;
+
+		if( abs( vProjected.y ) > vHalfBounds.y || abs( vProjected.z ) > vHalfBounds.z )
+			discard;
+
+		float distanceFromBoxStart = vProjected.x + vHalfBounds.x;
+		if( distanceFromBoxStart < 0.0 || distanceFromBoxStart > vDecalBounds.x )
+			discard;
+
+		float2 decalUV = (vProjected.yz / vDecalBounds.yz) + 0.5;
+
+		i.vTextureCoords.xy = 1.0 - decalUV;
+
+		i.vNormalWs = -i.vDecalRight;
+		i.vTangentUWs = -i.vDecalUp;
+		i.vTangentVWs = -i.vDecalForward;
+
+		float3 rotatedY = float3( -vProjected.z, vProjected.y, vProjected.x );
+		i.vPositionOs = float3( -rotatedY.x, -rotatedY.y, rotatedY.z );
+
+		i.vPositionWithOffsetWs.xyz = worldPos - g_vHighPrecisionLightingOffsetWs.xyz;
+
+		Material m = Material::Init( i );
+		m.Albedo = float3( 1, 1, 1 );
+		m.Normal = float3( 0, 0, 1 );
+		m.Roughness = 1;
+		m.Metalness = 0;
+		m.AmbientOcclusion = 1;
+		m.TintMask = 1;
+		m.Opacity = 1;
+		m.Emission = float3( 0, 0, 0 );
+		m.Transmission = 0;
+{11}
+		m.AmbientOcclusion = saturate( m.AmbientOcclusion );
+		m.Roughness = saturate( m.Roughness );
+		m.Metalness = saturate( m.Metalness );
+		m.Opacity = saturate( m.Opacity );
+		
+		// Result node takes normal as tangent space, convert it to world space now
+		m.Normal = m.Normal;
+
+		// Toolvis:
+		m.WorldTangentU = i.vTangentUWs;
+		m.WorldTangentV = i.vTangentVWs;
+		m.TextureCoords = i.vTextureCoords.xy;
+
+{12}
+	}}
+}}";
+}
